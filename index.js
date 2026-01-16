66a42ef802a202ab3ca41a60a878ca5292f098cb
Muhammad Farooq <fm6182218@gmail.com>
2026-01-16 23:33:53 +0500

    fs method in CRUD

---
+++ index.js
@@ -0,0 +1,48 @@
+console.log("Javascript connection");
+
+import { log } from "console";
+import fs from "fs"
+
+// sync
+// fs.writeFileSync("./test.txt", "Muhammad Farooq")
+
+// ================================
+// asycn
+// Add Data
+// fs.writeFile("./test.txt", "Hello Would", (err) => {
+//     if (err) throw err;
+//     console.log("Data Adding");
+// })
+// ================================
+
+// ================================
+// asycn
+// read Data
+// fs.readFile("./test.txt", (err, data) => {
+//     if (err) throw err
+//     console.log(data.toString());
+// })
+// ================================
+
+// ================================
+// asycn
+// Delete Data
+// fs.unlink("./test.txt", (err) => {
+//     if (err) throw err
+//     console.log("Delete Sucessfully");
+// })
+// ================================
+
+// ================================
+// async
+// update Data
+// fs.appendFile("./test.txt", "Hello NodeJs\r\n", (err) => {
+//     if (err) throw err;
+//     console.log("Updated Successfully");
+// });
+// ================================
+
+
+// import and export
+// import { add } from "./calc.js"
+// console.log(`Adding ${add(2, 2)}`);
