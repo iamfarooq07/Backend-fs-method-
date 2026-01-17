38f82147848a3f438e37460129b992824cbb4485
Muhammad Farooq <fm6182218@gmail.com>
2026-01-17 23:43:29 +0500

    Add Navigation fs and http

--- index.js
+++ index.js
@@ -1,19 +1,92 @@
 console.log("Javascript connection");
 import http from "http";
-// console.log(http);
+import fs from "fs";
 
-http.createServer((req, res) => {
-    res.write("Hello Would")
-    res.end()
+// // console.log(http);
+
+const result = http.createServer((req, res) => {
+    console.log("Request Done");
+
+
+    res.setHeader("Content-Type", "text/html");
+
+
+    switch (req.url) {
+        case "/":
+            //         res.end(`<h1>Welcome to Home Page</h1>
+            // <a href="/about">About</a>`);
+            fs.readFile("./index.html", (err, data) => {
+                if (err) throw err
+                console.log("Data Done");
+
+                res.end(data)
+            })
+            break;
+
+        case "/about":
+            //         res.end(`<h1>Welcome to About Page</h1>
+            // <a href="/">Home</a>`);
+            fs.readFile("./about.html", (err, data) => {
+                if (err) throw err;
+                console.log("About Data Done");
+                res.end(data)
+            })
+            break;
+
+        default:
+            // res.end("<h1>Page Not Found</h1>");
+
+            fs.readFile("./notfound.html", (err, data) => {
+                if (err) throw err;
+                res.end(data)
+            })
+
+    }
+
+
+
+    // if (req.url === "/") {
+    //     res.write("WellCome to Home Page")
+    //     res.end()
+    // } else if (req.url === "/about") {
+    //     res.write("WellCome to About Page")
+    //     res.end()
+    // } else {
+    //     res.write("Page Not Found")
+    //     res.end()
+    // }
+
+});
+
+let PORT = 3000;
+
+result.listen(PORT, () => {
+    console.log(`Server Hosting Link in http://localhost:${PORT}`);
+
 })
-    .listen(9002, () => {
-        console.log("Port Number 9002");
 
-    })
-// ================================
+
+// // ================================
 
 
 // import fs from "fs"
+
+// =================================
+
+// Folder Create and Folder Delete in fs method
+
+
+// fs.mkdir("views", (err) => {
+//     if (err) throw err
+// })
+
+// fs.rmdir("views", (err) => {
+//     if (err) throw err
+// })
+
+// =================================
+
+
 // sync
 // fs.writeFileSync("./test.txt", "Muhammad Farooq")
 
